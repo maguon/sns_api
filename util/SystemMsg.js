0444b96b4752f49e1c9f96cfa494f9b64280f8fa
yingmei567 <yingmei.yan@myxxjs.com>
2019-07-29 14:52:00 +0800

    Add module for user.

--- util/SystemMsg.js
+++ util/SystemMsg.js
@@ -1,13 +1,20 @@
 'use strict';
 let SYS_AUTH_TOKEN_ERROR ="当前用户级别，不能完成此操作" ;
-
+/**
+ * The module for admin
+ */
 let CUST_LOGIN_USER_PSWD_ERROR = "登录用户名或密码错误" ; // Customer enter a wrong password on login
 let CUST_SIGNUP_REGISTERED = "已被注册";//Cutomer do signup ,but the current phone has been exist in system.
 let CUST_LOGIN_PSWD_ERROR = "登录密码错误" ; // Customer enter a wrong password on login
+/**
+ * The module for user
+ */
+let USER_DELETE_INFO = "删除用户信息失败" ; // Failed to delete user information
 
 module.exports = {
     SYS_AUTH_TOKEN_ERROR,
     CUST_LOGIN_USER_PSWD_ERROR,
     CUST_SIGNUP_REGISTERED,
-    CUST_LOGIN_PSWD_ERROR
+    CUST_LOGIN_PSWD_ERROR,
+    USER_DELETE_INFO
 }
