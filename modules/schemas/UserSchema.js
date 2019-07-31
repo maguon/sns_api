9f31c9f3d5098c14ba34e44a5c77066d9616f950
yingmei567 <yingmei.yan@myxxjs.com>
2019-07-31 14:40:20 +0800

    Add "auth_status" and "type" and "last_login_on".

--- modules/schemas/UserSchema.js
+++ modules/schemas/UserSchema.js
@@ -6,7 +6,10 @@ const userSchema = new Schema({
         phone         : {type:Number,default:0,display: '用户电话'},
         password      : {type:String,default:'',trim:true,display: '用户密码'},
         nikename      : {type:String,default:'',trim:true,display: '用户昵称'},
-        status        : {type:Number,default:0,min:0,max:3,display: '验证状态'},
+        status        : {type:Number,default:0,min:0,max:3,display: '状态（1-使用 0-禁用）'},
+        auth_status   : {type:Number,default:0,min:0,max:3,display: '认证状态(0-未认证,1-已认证)'},
+        type          : {type:Number,default:0,min:0,max:3,display: '用户类型（1-普通，2-会员）'},
+        last_login_on : {type:Date,default:new Date(),display: '最后登录时间'},
         _userDetailId: {
             type: Schema.Types.ObjectId,
             ref: 'userDetail',
