c40b7611acaa3dcceeebb56faee77b6e6fbce948
yingmei567 <yingmei.yan@myxxjs.com>
2019-07-31 11:13:21 +0800

    Add _userDetailId.

--- modules/schemas/UserSchema.js
+++ modules/schemas/UserSchema.js
@@ -6,7 +6,12 @@ const userSchema = new Schema({
         phone         : {type:Number,default:0,display: '用户电话'},
         password      : {type:String,default:'',trim:true,display: '用户密码'},
         nikename      : {type:String,default:'',trim:true,display: '用户昵称'},
-        status        : {type:Number,default:0,min:0,max:3,display: '验证状态'}
+        status        : {type:Number,default:0,min:0,max:3,display: '验证状态'},
+        _userDetailId: {
+            type: Schema.Types.ObjectId,
+            ref: 'userDetail',
+            display: '用户详细信息ID'
+        },
     },
     { timestamps: { createdAt: 'created_at',updatedAt : 'updated_at' }
     });
