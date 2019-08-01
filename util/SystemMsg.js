df72bc35cab94c6300c4efab1178530c9ed572eb
yingmei567 <yingmei.yan@myxxjs.com>
2019-08-01 10:36:34 +0800

    Add CUST_ID_NULL_ERROR.

--- util/SystemMsg.js
+++ util/SystemMsg.js
@@ -1,9 +1,9 @@
 'use strict';
 let SYS_AUTH_TOKEN_ERROR ="当前用户级别，不能完成此操作" ;
-let SYS_OBJECT_ID_ERROR ="输入的ID格式不正确！" ;
 /**
  * The module for admin
  */
+let CUST_ID_NULL_ERROR ="该用户不存在！" ;
 let CUST_LOGIN_USER_PSWD_ERROR = "登录用户名或密码错误" ; // Customer enter a wrong password on login
 let CUST_SIGNUP_REGISTERED = "已被注册";//Cutomer do signup ,but the current phone has been exist in system.
 let CUST_LOGIN_PSWD_ERROR = "登录密码错误" ; // Customer enter a wrong password on login
@@ -19,8 +19,8 @@ let USER_DELETE_INFO = "删除用户信息失败" ; // Failed to delete user inf
 let MESSAGE_DELETE_ADMIN_ERROR = "该管理员无权删除此消息！" ; // The admin has no right to delete this message
 module.exports = {
     SYS_AUTH_TOKEN_ERROR,
-    SYS_OBJECT_ID_ERROR,
 
+    CUST_ID_NULL_ERROR,
     CUST_LOGIN_USER_PSWD_ERROR,
     CUST_SIGNUP_REGISTERED,
     CUST_LOGIN_PSWD_ERROR,
