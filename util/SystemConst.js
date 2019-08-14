c8f163e6de66aa256334ce076b74a0b6546d0a00
yingmei567 <yingmei.yan@myxxjs.com>
2019-08-14 09:35:06 +0800

    Delete del_status.

--- util/SystemConst.js
+++ util/SystemConst.js
@@ -5,12 +5,6 @@ const INFO_STATUS = {
         available:1//可用
     }
 }
-const DEL_STATIS = {
-    Status: {
-        not_deleted:0,//未删除
-        delete:1//已删除
-    }
-}
 const USER_ADDRESS ={
     type:{
         departure:1,
@@ -29,7 +23,6 @@ const READ_STATUS ={
 }
 module.exports = {
     INFO_STATUS,
-    DEL_STATIS,
     USER_ADDRESS,
     READ_STATUS
 }
