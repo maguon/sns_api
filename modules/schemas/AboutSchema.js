4a17428930cf03563d35b8f0bc78f68ff32d248e
yingmei567 <yingmei.yan@myxxjs.com>
2019-10-22 14:53:53 +0800

    Update phone type.

--- modules/schemas/AboutSchema.js
+++ modules/schemas/AboutSchema.js
@@ -5,8 +5,8 @@ const Schema = mongoose.Schema;
 const aboutSchema = new Schema({
         corporate_name  : {type:String,default:'',display: '公司名称'},
         info            : {type:String,default:'',display: '详细介绍'},
-        phone           : {type:Number,default:0,display: '电话'},
-        remarks        : {type:String,default:'',display: '备注'},
+        phone           : {type:String,default:'',display: '电话'},
+        remarks         : {type:String,default:'',display: '备注'},
         _adminId: {
             type: Schema.Types.ObjectId,
             ref: 'admin_user',
