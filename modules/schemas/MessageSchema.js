d586ee9c561f95e2186969e5a97011f1ce0071d4
yingmei567 <yingmei.yan@myxxjs.com>
2019-12-05 11:28:43 +0800

    Update createMessage media.

--- modules/schemas/MessageSchema.js
+++ modules/schemas/MessageSchema.js
@@ -13,9 +13,9 @@ const messageSchema = new Schema({
         agreeNum      : {type:Number,default:0,display: '点赞次数'},
         readNum       : {type:Number,default:0,display: '阅读次数'},
         label         : {type:String,default:'',trim:true,display: '消息标签'},
-        multi_media   : {type:String,default:'',trim:true,display: '图片/视频'},
+        media   : {type:Object,trim:true,display: '图片/视频'},
         status        : {type:Number,default:1,min:0,max:3,display: '状态（0-不可见，1-可见）'},
-        comment_status    : {type:Number,default:0,min:0,max:3,display: '评论状态(0-允许评论，1-不允许评论)'},
+        comment_status: {type:Number,default:0,min:0,max:3,display: '评论状态(0-允许评论，1-不允许评论)'},
         _userId: {
             type: Schema.Types.ObjectId,
             ref: 'user',
