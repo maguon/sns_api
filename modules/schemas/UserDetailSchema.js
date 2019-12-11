07709f87af7989853a074beae5c27da837c69ed6
yingmei567 <yingmei.yan@myxxjs.com>
2019-12-11 15:00:27 +0800

    Add numbers.

--- modules/schemas/UserDetailSchema.js
+++ modules/schemas/UserDetailSchema.js
@@ -4,12 +4,19 @@ const mongoose = require('../../db/connections/MongoCon.js').getMongo();
 
 const Schema = mongoose.Schema;
 const userDetailSchema = new Schema({
-        sex           : {type:Number,default:0,min:0,max:3,display: '性别（0-女 1-男）'},
-        nick_name     : {type:String,default:'',trim:true,display: '用户昵称'},
-        real_name     : {type:String,default:'',trim:true,display: '真实姓名'},
-        city_name     : {type:String,default:'',trim:true,display: '城市名称'},
-        intro         : {type:String,default:'',trim:true,display: '签名'},
-        avatar        : {type:String,default:'',trim:true,display: '用户头像'},
+        sex                     : {type:Number,default:0,min:0,max:3,display: '性别（0-女 1-男）'},
+        nick_name               : {type:String,default:'',trim:true,display: '用户昵称'},
+        real_name               : {type:String,default:'',trim:true,display: '真实姓名'},
+        city_name               : {type:String,default:'',trim:true,display: '城市名称'},
+        intro                   : {type:String,default:'',trim:true,display: '签名'},
+        avatar                  : {type:String,default:'',trim:true,display: '用户头像'},
+        messagesNum             : {type:Number,default:0,display: '发布文章数'},
+        followNum               : {type:Number,default:0,display: '我的关注数'},
+        attentionNum            : {type:Number,default:0,display: '被关注数'},
+        commentsNum             : {type:Number,default:0,display: '评论次数'},
+        voteNum                 : {type:Number,default:0,display: '参与投票数'},
+        messageCollectionNum    : {type:Number,default:0,display: '收藏文章数'},
+        locationCollectionNum   : {type:Number,default:0,display: '收藏位置数'},
         _userId: {
             type: Schema.Types.ObjectId,
             ref: 'user_info',
