c08b0f8eda0eeff586e74744ca264a17dbcd1b87
yingmei567 <yingmei.yan@myxxjs.com>
2019-12-18 15:06:11 +0800

    Add saveCollections.

--- controller/UserMessageCollectionController.js
+++ controller/UserMessageCollectionController.js
@@ -7,6 +7,7 @@ const sysConsts = require('../util/SystemConst');
 const logger = serverLogger.createLogger('UserMessageCollectionsController');
 
 const {UserMessageCollectionsModel} = require('../modules');
+const {UserDetailModel} = require('../modules');
 
 const getUserMessageCollections = (req, res, next) => {
     let params = req.query;
@@ -52,35 +53,71 @@ const createUserMessageCollections = (req, res, next) => {
     let bodyParams = req.body;
     let userMessageCollectionsObj = bodyParams;
     userMessageCollectionsObj.status = sysConsts.INFO.status.available;
-    if(path.userId){
-        if(path.userId.length == 24){
-            userMessageCollectionsObj._userId = mongoose.mongo.ObjectId(path.userId);
-        }else{
-            logger.info('createUserMessageCollections  userID format incorrect!');
-            resUtil.resetUpdateRes(res,null,systemMsg.CUST_ID_NULL_ERROR);
-            return next();
-        }
+    const saveCollections =()=>{
+        return new Promise((resolve, reject) => {
+            if(path.userId){
+                if(path.userId.length == 24){
+                    userMessageCollectionsObj._userId = mongoose.mongo.ObjectId(path.userId);
+                }else{
+                    logger.info('createUserMessageCollections  userID format incorrect!');
+                    resUtil.resetUpdateRes(res,null,systemMsg.CUST_ID_NULL_ERROR);
+                    return next();
+                }
+            }
+            if(path.messagesId){
+                if(path.messagesId.length == 24){
+                    userMessageCollectionsObj._messageId = mongoose.mongo.ObjectId(path.messagesId);
+                }else{
+                    logger.info('createUserMessageCollections  messagesId format incorrect!');
+                    resUtil.resetUpdateRes(res,null,systemMsg.MESSAGE_ID_NULL_ERROR);
+                    return next();
+                }
+            }
+            let userMessageCollectionsModel = new UserMessageCollectionsModel(userMessageCollectionsObj);
+            userMessageCollectionsModel.save(function(error,result){
+                if (error) {
+                    logger.error(' createUserMessageCollections ' + error.message);
+                    reject({err:error.message});
+                } else {
+                    logger.info(' createUserMessageCollections ' + 'success');
+                    resolve(result);
+                }
+            })
+        });
     }
-    if(path.messagesId){
-        if(path.messagesId.length == 24){
-            userMessageCollectionsObj._messageId = mongoose.mongo.ObjectId(path.messagesId);
-        }else{
-            logger.info('createUserMessageCollections  messagesId format incorrect!');
-            resUtil.resetUpdateRes(res,null,systemMsg.MESSAGE_ID_NULL_ERROR);
-            return next();
-        }
+
+    //更新用户投票数
+    const updateCollectionNum =(result)=>{
+        return new Promise(() => {
+            let queryUser = UserDetailModel.find({});
+            if(path.userId){
+                if(path.userId.length == 24){
+                    queryUser.where('_userId').equals(mongoose.mongo.ObjectId(path.userId));
+                }else{
+                    logger.info('createUserMessageCollections updateCollectionNum _userId format incorrect!');
+                    return next();
+                }
+            }
+            //文章收藏数加一
+            UserDetailModel.findOneAndUpdate(queryUser,{ $inc: { messageCollectionNum: 1 } }).exec((error,rows)=> {
+                if (error) {
+                    logger.error(' createUserMessageCollections updateCollectionNum ' + error.message);
+                } else {
+                    logger.info(' createUserMessageCollections updateCollectionNum ' + 'success');
+                    resUtil.resetCreateRes(res, result);
+                    return next();
+                }
+            });
+        });
     }
-    let userMessageCollectionsModel = new UserMessageCollectionsModel(userMessageCollectionsObj);
-    userMessageCollectionsModel.save(function(error,result){
-        if (error) {
-            logger.error(' createUserMessageCollections ' + error.message);
-            resUtil.resInternalError(error,res);
-        } else {
-            logger.info(' createUserMessageCollections ' + 'success');
-            resUtil.resetCreateRes(res, result);
-            return next();
-        }
-    })
+
+    saveCollections()
+        .then(updateCollectionNum)
+        .catch((reject)=>{
+            if(reject.err){
+                resUtil.resetFailedRes(res,reject.err);
+            }
+        })
 }
 const updateStatus = (req, res, next) => {
     let bodyParams = req.body;
