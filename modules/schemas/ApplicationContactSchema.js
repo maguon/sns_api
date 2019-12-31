27b85380911b131acd6d06f40217890c4090dbe5
yingmei567 <yingmei.yan@myxxjs.com>
2019-12-31 08:43:55 +0800

    Add remarks.

--- modules/schemas/ApplicationContactSchema.js
+++ modules/schemas/ApplicationContactSchema.js
@@ -4,6 +4,7 @@ const mongoose = require('../../db/connections/MongoCon.js').getMongo();
 const Schema = mongoose.Schema;
 const applicationContactSchema = new Schema({
         status          : {type:Number,default:0,display: '申请状态（0-未读，1-拒绝，2-同意）'},
+        remarks         : {type:String,default:'',display: '备注'},
         _userId: {
             type: Schema.Types.ObjectId,
             ref: 'user_info',
