9b3060d73e0b0950f1a3e2d1c616de474975e58b
yingmei567 <yingmei.yan@myxxjs.com>
2020-03-03 09:44:59 +0800

    Update address [lon,lat].

--- modules/schemas/MsgSchema.js
+++ modules/schemas/MsgSchema.js
@@ -6,7 +6,7 @@ const msgSchema = new Schema({
         type          : {type:Number,default:0,min:0,max:3,display: '消息类型(1.文章 2.求助 )'},
         carrier       : {type:Number,default:0,min:0,max:5,display: '载体类型(1.文本 2.图片 3.视频 4.位置分享 )'},
         info          : {type:String,default:'',trim:true,display: '内容'},
-        address       : {type:Array,index: {type: '2d',sparse: true},display: '发布文章-地理位置(格式： [经度, [经度，纬度]])'},
+        address       : {type:Array,index: {type: '2d',sparse: true},display: '发布文章-地理位置(格式：[经度，纬度])'},
         address_name  : {type:String,default:'',trim:true,display: '发布文章-地理位置名称'},
         address_real  : {type:String,default:'',trim:true,display: '发布文章-真实地址'},
         address_show  : {type:Number,default:0,min:0,max:3,display: '发布文章-位置显示状态(0-不可见，1-可见)'},
