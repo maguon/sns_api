a8d61bb4a1ad32a7b46c2116999295ad25bc16c2
yingmei567 <yingmei.yan@myxxjs.com>
2020-06-17 15:43:57 +0800

    Update valid_results default.

--- modules/schemas/ReportSchema.js
+++ modules/schemas/ReportSchema.js
@@ -4,7 +4,7 @@ const mongoose = require('../../db/connections/MongoCon.js').getMongo();
 const Schema = mongoose.Schema;
 const reportSchema = new Schema({
         status          : {type:Number,default:1,min:0,max:3,display: '处理状态（1-未处理（默认），2-已处理）'},
-        valid_results   : {type:Number,default:0,min:0,max:3,display: '处理结果（1-有效，2-无效）'},
+        valid_results   : {type:Number,default:2,min:0,max:3,display: '处理结果（1-有效，2-无效(默认)）'},
         remarks         : {type:String,default:'',display: '说明'},
         review          : {type:String,default:'',display: '管理员处理说明'},
         review_time     : {type:Date,display: '处理时间'},
