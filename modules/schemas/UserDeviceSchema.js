8aea7e316e0acd0ab59264291ce82aba21a92489
yingmei567 <yingmei.yan@myxxjs.com>
2020-07-29 08:47:39 +0800

    Update version display.

--- modules/schemas/UserDeviceSchema.js
+++ modules/schemas/UserDeviceSchema.js
@@ -4,7 +4,7 @@ const mongoose = require('../../db/connections/MongoCon.js').getMongo();
 const Schema = mongoose.Schema;
 const userDeviceSchema = new Schema({
         device_token  : {type:String,default:'',trim:true,display: '设备标号'},
-        version       : {type:String,default:'',trim:true,display: '设备版本'},
+        version       : {type:String,default:'',trim:true,display: 'app版本'},
         app_type      : {type:Number,default:0,min:0,max:3,display: 'app登录类型（1-司机之家）'},
         device_type   : {type:Number,default:0,min:0,max:3,display: '设备类型(1-android,2-ios)'},
         status        : {type:Number,default:0,min:0,max:3,display: '设备登录状态(-1-退出登录，1-登录中)'},
