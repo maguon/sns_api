9f75d4657f9e3102e8c42a73df5bc4c2a8006a1d
yingmei567 <yingmei.yan@myxxjs.com>
2020-09-29 15:25:24 +0800

    Add usable.

--- modules/schemas/AdminMenuListSchema.js
+++ modules/schemas/AdminMenuListSchema.js
@@ -5,10 +5,11 @@ const Schema = mongoose.Schema;
 const adminMenuListSchema = new Schema({
         type          : {type:String,default:0,display: '管理员类型(99-超级管理员, 1-其他)'},
         menu_list     : [{
-            label    : {type:String,default:'',trim:true,display: '标签'},
-            icon     : {type:String,default:'',trim:true,display: '图标'},
-            link     : {type:String,trim:true,display: '链接'},
-            children : {type:Array,default:'',trim:true,display: ''}
+            label     : {type:String,default:'',trim:true,display: '标签'},
+            icon      : {type:String,default:'',trim:true,display: '图标'},
+            link      : {type:String,trim:true,display: '链接'},
+            children  : {type:Array,default:'',trim:true,display: ''},
+            usable    : {type:Boolean,default:true}
         }],
     },
     { timestamps: { createdAt: 'created_at',updatedAt : 'updated_at' }
